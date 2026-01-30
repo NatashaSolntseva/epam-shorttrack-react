c98375fd49f13caadf5e2c85b0daa4a78240323c
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2026-01-30 16:50:11 +0300

    feat: empty list component add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import { Box, Container } from '@mui/material';
 import { Header } from './components/Header/Header';
 //import { CoursesToolbar } from './components/CoursesToolbar/CoursesToolbar';
-import {
-  mockedCoursesList,
-  mockedAuthorsList,
-} from './__mocks__/coursesList.ts';
+// import {
+//   mockedCoursesList,
+//   mockedAuthorsList,
+// } from './__mocks__/coursesList.ts';
 // import { CoursesList } from './components/CoursesList/CoursesList.tsx';
-import { CourseInfo } from './components/CourseInfo/CourseInfo.tsx';
+//import { CourseInfo } from './components/CourseInfo/CourseInfo.tsx';
+import { EmptyCoursesList } from './components/EmptyCoursesList/EmptyCoursesList.tsx';
 
 function App() {
-  const authorsById = Object.fromEntries(
-    mockedAuthorsList.map((a) => [a.id, a.name] as const)
-  );
+  // const authorsById = Object.fromEntries(
+  //   mockedAuthorsList.map((a) => [a.id, a.name] as const)
+  // );
 
-  const selectedCourse = {
-    ...mockedCoursesList[0],
-    authors: mockedCoursesList[0].authors.map(
-      (id) => authorsById[id] ?? 'Unknown author'
-    ),
-  };
+  // const selectedCourse = {
+  //   ...mockedCoursesList[0],
+  //   authors: mockedCoursesList[0].authors.map(
+  //     (id) => authorsById[id] ?? 'Unknown author'
+  //   ),
+  // };
 
-  const handleBack = () => console.log('back');
+  //const handleBack = () => console.log('back');
+  const handleAdd = () => console.log('add new course');
   return (
     <>
       <Header />
@@ -33,7 +35,8 @@ function App() {
         </Container> */}
         {/* <CoursesList courses={mockedCoursesList} authors={mockedAuthorsList} /> */}
         <Container maxWidth="lg" sx={{ mt: 3 }}>
-          <CourseInfo course={selectedCourse} onBack={handleBack} />
+          {/* <CourseInfo course={selectedCourse} onBack={handleBack} /> */}
+          <EmptyCoursesList onAdd={handleAdd} />
         </Container>
       </Box>
     </>
---
+++ src/components/EmptyCoursesList/Title.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+export function Title() {
+  return (
+    <Typography variant="h5" sx={{ fontWeight: 700, color: 'text.primary' }}>
+      Your List Is Empty
+    </Typography>
+  );
+}
